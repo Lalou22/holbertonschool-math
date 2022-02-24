a7d2e7e9778d5ec4f1281779b0dbb31e91575eb1
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-02-24 17:20:59 -0600

    Task 5

---
+++ 0x00-math_complex/5-substraction.c
@@ -0,0 +1,19 @@
+/*
+ * File: 4-addition.c
+ * Auth: Lalo Rdz
+ */
+#include "holberton.h"
+/**
+ * substraction - Function performs the substraction operation to c-numbers.
+ * @c1: First Complex number a + ib represented by two doubles.
+ * @c2: Second Complex number a + ib represented by two doubles.
+ * @c3: Pointer to the third Complex number a + ib represented by two doubles.
+ *
+ * Description: Subs real part of c1 and c2. Subs imaginary part of c1 and c2.
+ * Return: Void.
+ */
+void substraction(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re - c2.re;
+	c3->im = c1.im - c2.im;
+}
